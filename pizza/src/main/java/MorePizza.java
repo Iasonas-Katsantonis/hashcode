220214d83f1e82c8f3014991012959c7b3ffff1b
M. Waisberg <pnemonic@gmail.com>
2020-02-24 10:24:54 +0200

    threads.

--- pizza/src/main/java/MorePizza.java
+++ pizza/src/main/java/MorePizza.java
@@ -1,24 +1,45 @@
 import java.io.File;
 import java.io.IOException;
 
-public class MorePizza {
+public class MorePizza implements Runnable {
+
+    private final String name;
+    private final File fileIn;
+    private final File fileOut;
+
+    public MorePizza(String name, File fileIn, File fileOut) {
+        this.name = name;
+        this.fileIn = fileIn;
+        this.fileOut = fileOut;
+    }
+
+    @Override
+    public void run() {
+        try {
+            solve(fileIn, fileOut);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
     private void solve(File fileIn, File fileOut) throws IOException {
-        SolverRecursive solver = new SolverRecursive();
+        SolverStack solver = new SolverStack();
         Solution solution = solver.solve(fileIn);
-        System.out.println("solution: " + solution);
-        System.out.println("delta: " + solution.score());
+        System.out.println(name + ": solution: " + solution.score());
         new SolutionWriter().write(solution, fileOut);
     }
 
-    public static void main(String[] args) throws Exception {
-        String filename = args[0];
-        File fileIn = new File(filename);
-        int indexExt = filename.lastIndexOf('.');
-        if (indexExt >= 0) {
-            filename = filename.substring(0, indexExt);
+    public static void main(String[] args) {
+        for (String arg : args) {
+            String filename = arg;
+            File fileIn = new File(filename);
+            int indexExt = filename.lastIndexOf('.');
+            if (indexExt >= 0) {
+                filename = filename.substring(0, indexExt);
+            }
+            File fileOut = new File(filename + ".out");
+            MorePizza slices = new MorePizza(filename, fileIn, fileOut);
+            new Thread(slices).start();
         }
-        File fileOut = new File(filename + ".out");
-        new MorePizza().solve(fileIn, fileOut);
     }
 }
--- pizza/src/main/java/MorePizzaRecursive.java
+++ pizza/src/main/java/MorePizzaRecursive.java
@@ -1,24 +1,45 @@
 import java.io.File;
 import java.io.IOException;
 
-public class MorePizzaRecursive {
+public class MorePizzaRecursive implements Runnable {
+
+    private final String name;
+    private final File fileIn;
+    private final File fileOut;
+
+    public MorePizzaRecursive(String name, File fileIn, File fileOut) {
+        this.name = name;
+        this.fileIn = fileIn;
+        this.fileOut = fileOut;
+    }
+
+    @Override
+    public void run() {
+        try {
+            solve(fileIn, fileOut);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
     private void solve(File fileIn, File fileOut) throws IOException {
         SolverRecursive solver = new SolverRecursive();
         Solution solution = solver.solve(fileIn);
-        System.out.println("solution: " + solution);
-        System.out.println("delta: " + solution.score());
+        System.out.println(name + ": solution: " + solution.score());
         new SolutionWriter().write(solution, fileOut);
     }
 
-    public static void main(String[] args) throws Exception {
-        String filename = args[0];
-        File fileIn = new File(filename);
-        int indexExt = filename.lastIndexOf('.');
-        if (indexExt >= 0) {
-            filename = filename.substring(0, indexExt);
+    public static void main(String[] args) {
+        for (String arg : args) {
+            String filename = arg;
+            File fileIn = new File(filename);
+            int indexExt = filename.lastIndexOf('.');
+            if (indexExt >= 0) {
+                filename = filename.substring(0, indexExt);
+            }
+            File fileOut = new File(filename + ".out");
+            MorePizzaRecursive slices = new MorePizzaRecursive(filename, fileIn, fileOut);
+            new Thread(slices).start();
         }
-        File fileOut = new File(filename + ".out");
-        new MorePizzaRecursive().solve(fileIn, fileOut);
     }
 }
