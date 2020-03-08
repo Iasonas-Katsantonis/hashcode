29ea4ec78c57a3616631cf1a6cc22c30a3c1038c
M. Waisberg <pnemonic@gmail.com>
2020-03-08 14:48:28 +0200

    try pick by book desc - library already scanning?

--- hashcode2020_qualification_task/src/main/java/Solver3.java
+++ hashcode2020_qualification_task/src/main/java/Solver3.java
@@ -1,6 +1,7 @@
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.HashMap;
@@ -8,6 +9,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.SortedMap;
 import java.util.TreeMap;
+import java.util.TreeSet;
 
 public class Solver3 {
     public Solution solve(File fileIn) throws IOException {
@@ -23,37 +25,65 @@ public class Solver3 {
         Map<Book, List<Library>> librariesPerBook = getLibrariesPerBook(problem);
         List<Library> libraries;
         Library library;
-        boolean libraryScanning;
         int daysRemaining = problem.D;
         Map<Library, Integer> daysPerLibrary = new HashMap<>();
         int days;
+        Collection<Book> booksUnused = new TreeSet<>(librariesPerBook.keySet());
+        boolean scanned;
 
         for (Book book : librariesPerBook.keySet()) {
             libraries = librariesPerBook.get(book);
-            while (libraries.size() > 0) {
-                library = libraries.remove(0);
-                libraryScanning = daysPerLibrary.containsKey(library);
-                days = libraryScanning ? daysPerLibrary.get(library) : daysRemaining;
+            scanned = false;
+
+            library = findLibraryScanning(libraries, librariesSolution);
+            while (!scanned && (library != null)) {
+                libraries.remove(library);
+                days = daysPerLibrary.get(library);
                 if (library.scan(book, days)) {
-                    if (!libraryScanning) {
-                        librariesSolution.add(library);
-                        daysPerLibrary.put(library, days);
-                        daysRemaining -= library.T;
-                    }
-                    break;
+                    scanned = true;
+                    booksUnused.remove(book);
+                } else {
+                    library = findLibraryScanning(libraries, librariesSolution);
+                }
+            }
+            while (!scanned && (libraries.size() > 0)) {
+                library = libraries.remove(0);
+                days = daysRemaining;
+                if (library.scan(book, days)) {
+                    scanned = true;
+                    booksUnused.remove(book);
+                    librariesSolution.add(library);
+                    daysPerLibrary.put(library, days);
+                    daysRemaining -= library.T;
                 }
             }
         }
 
+        System.err.println("unused: " + booksUnused.size());
         return solution;
     }
 
+    private Library findLibraryScanning(List<Library> libraries, List<Library> solution) {
+        if (libraries.isEmpty() || solution.isEmpty()) {
+            return null;
+        }
+        for (Library library : libraries) {
+            if (solution.contains(library)) {
+                return library;
+            }
+        }
+        return null;
+    }
+
     private SortedMap<Book, List<Library>> getLibrariesPerBook(Problem problem) {
+        List<Library> librariesProblem = new ArrayList<>(problem.libraries);
         SortedMap<Book, List<Library>> librariesPerBook = new TreeMap<>();
         List<Library> libraries;
         Comparator<Library> librarySorter = new SortLibraries3();
 
-        for (Library library : problem.libraries) {
+        Collections.sort(librariesProblem, librarySorter);
+
+        for (Library library : librariesProblem) {
             library.init(problem.D);
             for (Book book : library.books) {
                 libraries = librariesPerBook.get(book);
@@ -62,7 +92,6 @@ public class Solver3 {
                     librariesPerBook.put(book, libraries);
                 }
                 libraries.add(library);
-                Collections.sort(libraries, librarySorter);
             }
         }
 
