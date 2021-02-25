f4cfaae2e3801b39538ce7c1ec3807678acacec8
M. Waisberg <pnemonic@gmail.com>
2021-02-25 21:47:58 +0200

    Traffic signaling - solve

--- cars.2021/src/main/java/Path.kt
+++ cars.2021/src/main/java/Path.kt
@@ -1,1 +1,7 @@
-data class Path(val index: Int, val streets: List<Street>)
+data class Path(val index: Int, val streets: List<Street>) : Comparable<Path> {
+    override fun compareTo(other: Path): Int {
+        val c = this.streets.size - other.streets.size
+        if (c != 0) return c
+        return this.index - other.index
+    }
+}
--- cars.2021/src/main/java/Solver1.kt
+++ cars.2021/src/main/java/Solver1.kt
@@ -1,14 +1,21 @@
+import java.util.Comparator
+
 open class Solver1 {
 
     open fun solve(problem: Problem): Solution {
         val solution = Solution(problem)
+        val cars = ArrayList(problem.cars)
+        if (cars.isEmpty()) {
+            return solution
+        }
+        cars.sortWith(CarSorter())
 
         val maxTime = problem.simulationDuration
         var time = 0
         var intersection: Intersection
         val intersections = HashMap<Int, Intersection>()
 
-        for (car in problem.cars) {
+        for (car in cars) {
             for (street in car.path.streets) {
                 val intersectionId = street.endIntersection
                 var inter = intersections[intersectionId]
@@ -20,13 +27,24 @@ open class Solver1 {
                 intersection.schedule[street] = 1
 
                 time += street.duration
-                if (time >= maxTime) break
+                // TODO if (time >= maxTime) break
             }
-            if (time >= maxTime) break
+            // TODO if (time >= maxTime) break
         }
 
         solution.intersections.addAll(intersections.values)
 
         return solution
     }
+
+
+    /** Smaller path first. */
+    inner class CarSorter : Comparator<Car> {
+        override fun compare(o1: Car, o2: Car): Int {
+            val p1 = o1.path
+            val p2 = o2.path
+            return p1.compareTo(p2)
+        }
+    }
+
 }
